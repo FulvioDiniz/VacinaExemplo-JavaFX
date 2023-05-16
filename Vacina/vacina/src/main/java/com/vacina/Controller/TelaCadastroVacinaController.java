a7b488a8789fb3a0f54677e1cf913cefc5df6239
FulvioDiniz <fulviodiniz0@gmail.com>
2023-05-16 10:45:00 -0300

    ver1

---
+++ Vacina/vacina/src/main/java/com/vacina/Controller/TelaCadastroVacinaController.java
@@ -0,0 +1,51 @@
+package com.vacina.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class TelaCadastroVacinaController {
+
+    @FXML
+    private Button buttoncadastrar;
+
+    @FXML
+    private Button buttonfechar;
+
+    @FXML
+    private Button buttonlimpar;
+
+    @FXML
+    private TextArea textareadescricao;
+
+    @FXML
+    private TextField textfieldcodigo;
+
+    @FXML
+    private TextField textfieldcodigo1;
+
+    @FXML
+    private Label textfieldnome;
+
+    @FXML
+    private Label textfieldnome1;
+
+    @FXML
+    void cadastrarclicado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fecharclicado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void limparclicado(ActionEvent event) {
+
+    }
+
+}
--- Vacina/vacina/src/main/java/module-info.java
+++ Vacina/vacina/src/main/java/module-info.java
@@ -1,7 +1,9 @@
-module com.vacina {
+module JavaFXApplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.vacina to javafx.fxml;
+    opens com.vacina.Controller to javafx.fxml, javafx.graphics;
     exports com.vacina;
 }
